f08d3b8b742446492f3a6c455bb57045be1fccb7
caidapao <1014888954@qq.com>
2019-11-26 00:09:28 +0800

    1.将febs-shiro的代码复制过来了，直接用此模板开发博客系统 2.以后再考虑做自己的博客系统

---
+++ src/main/resources/static/febs/lay/modules/api.js
@@ -0,0 +1,6 @@
+layui.define([],function(exports){
+    console.log("============api.js===========");
+    exports('api',{
+        getMenus: 'menu/' + currentUser.username + '?invalid_ie_cache=' + new Date().getTime()
+    });
+});
--- src/main/resources/static/today/lay/modules/api.js
+++
@@ -1,6 +0,0 @@
-layui.define([],function(exports){
-    console.log("============api.js============");
-    exports('api',{
-        getMenus: 'menu/' + currentUser.username + '?invalid_ie_cache=' + new Date().getTime()
-    });
-});
